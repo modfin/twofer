d7f8f46b9ba7fdb43b6755a307498f98124882ff
Rasmus Holm <rasmus.holm@modularfinance.se>
2020-02-20 18:01:55 +0100

    cleanup

--- models.go
+++
@@ -1,9 +0,0 @@
-package twofer
-
-import "context"
-
-type PubSub interface {
-	Notify(topic string, payload []byte) error
-	Listen(ctx context.Context, topic string) (reader <-chan []byte, cancel func())
-	Next(ctx context.Context, topic string) ([]byte, error)
-}
