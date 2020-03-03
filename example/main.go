9909e7762a4b5d42700334ff83f1e40d30cd8234
David Odelberg <david.odelberg@modularfinance.se>
2020-03-03 17:28:45 +0100

    Adds a very simple js client
    - The client itself is created with two parameters, one for the register
    url of the RP and one for the login url of the RP.
    
    - When calling register it does a GET towards the RP for getting all the
    data necessary for creating a key pair and hands over responsibility to
    the authenticator. Whatever comes back is then sent back to the RP in
    the expected format by making a POST to the same endpoint.
    
    - The login works in similar wat by making a GET request for getting
    the necessary data to perform login. That is handed over to the
    authenticator and what comes back is sent to the same endpoint with
    POST.
    
    All in all, this client assumes the RP listens for GET and POST on the
    same url.

--- example/main.go
+++ example/main.go
@@ -39,10 +39,10 @@ func main() {
 	_client = gw6n.NewWebAuthnClient(conn)
 
 	m := mux.NewRouter()
-	m.HandleFunc("/register/{userId}", registerBegin).Methods("GET")
-	m.HandleFunc("/register/finish/{userId}", registerFinish).Methods("POST")
-	m.HandleFunc("/login/begin/{userId}", loginBegin).Methods("GET")
-	m.HandleFunc("/login/finish/{userId}", loginFinish).Methods("POST")
+	m.HandleFunc("/register", registerBegin).Methods("GET")
+	m.HandleFunc("/register", registerFinish).Methods("POST")
+	m.HandleFunc("/login", loginBegin).Methods("GET")
+	m.HandleFunc("/login", loginFinish).Methods("POST")
 	m.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./static"))))
 
 	http.Handle("/", m)
@@ -50,8 +50,7 @@ func main() {
 }
 
 func loginFinish(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	userId := vars["userId"]
+	userId := r.Header.Get("user")
 	u, ok := dao.Get(userId)
 
 	if !ok {
@@ -79,9 +78,8 @@ func loginFinish(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func loginBegin(w http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	userId := vars["userId"]
+func loginBegin(w http.ResponseWriter, r *http.Request) {
+	userId := r.Header.Get("user")
 	u, ok := dao.Get(userId)
 	if !ok {
 		httpError(w, errors.New("no user "+userId))
@@ -97,13 +95,13 @@ func loginBegin(w http.ResponseWriter, request *http.Request) {
 
 	w.Header().Set("WebAuthn-Session", string(resp.Session))
 	w.Header().Set("Content-Type:", "application/json")
+	fmt.Printf("%s", string(resp.Json))
+
 	_, _ = w.Write(resp.Json)
 }
 
 func registerBegin(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	userId := vars["userId"]
-
+	userId := r.Header.Get("user")
 	u, _ := dao.Get(userId)
 
 	resp, err := _client.BeginRegister(context.Background(), &gw6n.BeginRegisterRequest{
@@ -120,12 +118,12 @@ func registerBegin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("WebAuthn-Session", string(resp.Session))
 	w.Header().Set("Content-Type:", "application/json")
 	w.WriteHeader(200)
+	fmt.Printf("%s", string(resp.Json))
 	_, _ = w.Write(resp.Json)
 }
 
 func registerFinish(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	userId := vars["userId"]
+	userId := r.Header.Get("user")
 	u, _ := dao.Get(userId)
 
 	session := []byte(r.Header.Get("WebAuthn-Session"))
