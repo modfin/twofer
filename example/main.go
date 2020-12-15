1bf8b8b2231669bb0fe484b5e6eae390f69db0d8
Rasmus Holm <rasmus.holm@modularfinance.se>
2020-12-15 12:44:57 +0100

    refactoring

--- example/main.go
+++ example/main.go
@@ -10,17 +10,24 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"twofer/example/eid/dao"
 	"twofer/grpc/geid"
 	"twofer/grpc/gqr"
+	"twofer/grpc/gw6n"
 )
 
 var (
-	serverAddr = "twoferd:43210"
-	_eidClient geid.EIDClient
-	_qrQlient  gqr.QRClient
+	serverAddr   = "twoferd:43210"
+	_eidClient   geid.EIDClient
+	_qrQlient    gqr.QRClient
+	_authnClient gw6n.WebAuthnClient
 )
 
+func httpError(writer http.ResponseWriter, err error) {
+	writer.WriteHeader(500)
+	writer.Write([]byte(err.Error()))
+}
 func main() {
 
 	var opts []grpc.DialOption
@@ -35,18 +42,24 @@ func main() {
 	defer conn.Close()
 	_eidClient = geid.NewEIDClient(conn)
 	_qrQlient = gqr.NewQRClient(conn)
-
+	_authnClient = gw6n.NewWebAuthnClient(conn)
 	m := mux.NewRouter()
-	m.HandleFunc("/providers", getProviders)
 
-	m.HandleFunc("/inferred", initInferred)
-	m.HandleFunc("/start-auth", ssnAuthInit)
-	m.HandleFunc("/start-sign", ssnSignInit)
+	// Eid
+	m.HandleFunc("/eid/providers", getProviders)
+	m.HandleFunc("/eid/inferred", initInferred)
+	m.HandleFunc("/eid/start-auth", ssnAuthInit)
+	m.HandleFunc("/eid/start-sign", ssnSignInit)
+	m.HandleFunc("/eid/qrimage/{ref}", qrImage)
+	m.HandleFunc("/eid/peek/{ref}", peek)
+	m.HandleFunc("/eid/collect/{ref}", collect)
+	m.HandleFunc("/eid/cancel", cancel)
 
-	m.HandleFunc("/qrimage/{ref}", qrImage)
-	m.HandleFunc("/peek/{ref}", peek)
-	m.HandleFunc("/collect/{ref}", collect)
-	m.HandleFunc("/cancel", cancel)
+	// Twofer
+	m.HandleFunc("/authn/register", registerBegin).Methods("GET")
+	m.HandleFunc("/authn/register", registerFinish).Methods("POST")
+	m.HandleFunc("/authn/login", loginBegin).Methods("GET")
+	m.HandleFunc("/authn/login", loginFinish).Methods("POST")
 
 	m.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./static"))))
 
@@ -187,8 +200,7 @@ func collect(w http.ResponseWriter, r *http.Request) {
 		internalError(w, err)
 		return
 	}
-	res := AuthResult{Status: collect.Status.String()}
-	ar, err := json.Marshal(res)
+	ar, err := json.Marshal(collect)
 	if err != nil {
 		internalError(w, err)
 		return
@@ -266,6 +278,147 @@ func qrImage(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(qr.Image)
 }
 
-type AuthResult struct {
-	Status string `json:"status"`
+func createConfig(r *http.Request) *gw6n.Config {
+	if r.Host != "localhost:8080" {
+		name := strings.Split(r.Host, ":")[0]
+		return &gw6n.Config{
+			RPID:          name,
+			RPDisplayName: name,
+			RPOrigin:      "http://" + r.Host,
+		}
+	}
+	return nil
+}
+
+func loginBegin(w http.ResponseWriter, r *http.Request) {
+	var cfg = createConfig(r)
+
+	userId := r.Header.Get("user")
+	u, ok := dao.Get(userId)
+	if !ok {
+		httpError(w, errors.New("no user "+userId))
+		return
+	}
+	resp, err := _authnClient.AuthInit(context.Background(), &gw6n.AuthInitReq{
+		// The credentials for the user provided in the latest performed enrollment finish step
+		UserBlob: u.Blob,
+
+		// Optional, will use server default if not set
+		Cfg: cfg,
+	})
+	if err != nil {
+		httpError(w, err)
+		return
+	}
+
+	// The session will be used to create the credentials and must be provided in the finish step.
+	// While setting it as a header or a cookie works, the session can become quite big
+	// and we suggest you persist it in a session store where it can later be retrived, eg. Redis, memcached or such.
+	w.Header().Set("WebAuthn-Session", string(resp.Session))
+
+	w.Header().Set("Content-Type:", "application/json")
+	fmt.Printf("%s", string(resp.Json))
+
+	_, _ = w.Write(resp.Json)
+}
+
+func loginFinish(w http.ResponseWriter, r *http.Request) {
+	var cfg = createConfig(r)
+
+	session := []byte(r.Header.Get("WebAuthn-Session"))
+	signature, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		httpError(w, err)
+		return
+	}
+
+	_, err = _authnClient.AuthFinal(context.Background(), &gw6n.FinalReq{
+		// The session that was created in the registerBegin begin step
+		Session: session,
+
+		// The signature provided from the frontend.
+		Signature: signature,
+
+		// Optional, will use server default if not set
+		Cfg: cfg,
+	})
+	if err != nil {
+		httpError(w, err)
+		return
+	}
+
+}
+
+func registerBegin(w http.ResponseWriter, r *http.Request) {
+
+	var cfg = createConfig(r)
+
+	userId := r.Header.Get("user")
+	u, _ := dao.Get(userId)
+
+	resp, err := _authnClient.EnrollInit(context.Background(), &gw6n.EnrollInitReq{
+		// This is primarily used for first enrollment.
+		// if the user does exist, only adding the blob works fine
+		User: &gw6n.User{
+			Id: userId,
+		},
+
+		// if the user already have credentials and want to enroll new once, attach the current one
+		UserBlob: u.Blob,
+
+		// Optional, will use server default if not set
+		Cfg: cfg,
+	})
+	if err != nil {
+		httpError(w, err)
+		return
+	}
+
+	// The session will be used to create the credentials and must be provided in the finish step.
+	// While setting it as a header or a cookie works, the session can become quite big
+	// and we suggest you persist it in a session store where it can later be retrived, eg. Redis, memcached or such.
+	w.Header().Set("WebAuthn-Session", string(resp.Session))
+	w.Header().Set("Content-Type:", "application/json")
+	w.WriteHeader(200)
+	fmt.Printf("%s", string(resp.Json))
+	_, _ = w.Write(resp.Json)
+}
+
+func registerFinish(w http.ResponseWriter, r *http.Request) {
+
+	var cfg = createConfig(r)
+
+	userId := r.Header.Get("user")
+
+	session := []byte(r.Header.Get("WebAuthn-Session"))
+	signature, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		httpError(w, err)
+		return
+	}
+	resp, err := _authnClient.EnrollFinal(context.Background(), &gw6n.FinalReq{
+		// The session that was created in the registerBegin begin step
+		Session: session,
+
+		// The signature provided from the frontend.
+		Signature: signature,
+
+		// Optional, will use server default if not set
+		Cfg: cfg,
+	})
+
+	if err != nil {
+		httpError(w, err)
+		return
+	}
+
+	dao.Set(dao.User{
+		Id: userId,
+
+		//Make sure to persist the UserBlob along with the user. This Blob is needed to later authenticate the user.
+		Blob: resp.UserBlob,
+	})
+
+	w.WriteHeader(200)
 }
