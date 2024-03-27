3220f48c3289194272ab29b0f98966f56b316703
Lars-Erik Svensson <lars-erik.svensson@modularfinance.se>
2024-03-27 13:17:30 +0100

    Q&D refactor to also send back status and hint changes over SSE

--- internal/sse/sender.go
+++ internal/sse/sender.go
@@ -1,15 +1,19 @@
 package sse
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/labstack/echo/v4"
 	"net/http"
+	"sync/atomic"
+
+	"github.com/labstack/echo/v4"
 )
 
 // Sender represents a http server-sent events sender used to send compatible SSE events.
 // See https://html.spec.whatwg.org/multipage/server-sent-events.html
 type Sender struct {
+	eventID  atomic.Uint32
 	response *echo.Response
 	writer   http.ResponseWriter
 	flusher  http.Flusher
@@ -35,24 +39,30 @@ func (s *Sender) Prepare() {
 	s.writer.Header().Set("Transfer-Encoding", "chunked")
 }
 
-type Event struct {
-	Id    string `json:"id"`
-	Event string `json:"event"`
-	Data  string `json:"data"`
-}
+// type Event struct {
+// 	Id    string `json:"id"`
+// 	Event string `json:"event"`
+// 	Data  string `json:"data"`
+// }
 
-func (s *Sender) Send(event Event) error {
-	_, err := fmt.Fprintf(s.writer, "id: %s\n", event.Id)
+func (s *Sender) Send(event string, data any) error {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("ERR: failed to marshal event message: %v\n", err)
+		return err
+	}
+
+	_, err = fmt.Fprintf(s.writer, "id: %d\n", s.eventID.Add(1)-1)
 	if err != nil {
 		return err
 	}
 
-	_, err = fmt.Fprintf(s.writer, "event: %s\n", event.Event)
+	_, err = fmt.Fprintf(s.writer, "event: %s\n", event)
 	if err != nil {
 		return err
 	}
 
-	_, err = fmt.Fprintf(s.writer, "data: %s\n", event.Data)
+	_, err = fmt.Fprintf(s.writer, "data: %s\n", bytes)
 	if err != nil {
 		return err
 	}
